1e56a2ee9598dc55852bb3c5dd987b5f7adc609e
Sarah Dang <sarah.nldang@gmail.com>
2020-05-01 01:32:14 -0700

    check 1 2 :angel:

---
+++ contactform.php
@@ -0,0 +1,25 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $visitor_email = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $emailFrom = "sarah.nldang@gmail.com";
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "Name: $name.\n".
+                    "Email: $visitor_email.\n".
+                        "Message: $message.\n";
+
+    $to = "sarah.nldang@gmail.com";
+
+    $headers = "From: .$emailFrom \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+    
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+}
